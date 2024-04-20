2fc69233c7aac4bd4df11b5663d3915aea8f668d
vvvrdo <vvvrdo@gmail.com>
2024-04-20 16:34:56 +0400

    Set up React Router and Redux configuration

--- apps/movies/src/renderer/src/main.tsx
+++ apps/movies/src/renderer/src/main.tsx
@@ -2,10 +2,30 @@ import './assets/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { store } from './store.js'
+import { Provider } from 'react-redux'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './error-page'
+import Root from './routes/root'
+import Details from './routes/movie-details'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: 'details/:IMDBId',
+    element: <Details />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
