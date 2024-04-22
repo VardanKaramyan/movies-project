5fb76e7309fbfd7b0193f1d671e5be56760135a9
vvvrdo <vvvrdo@gmail.com>
2024-04-22 05:05:05 +0400

    Create SDK tools for API

---
+++ packages/IMDbOT-SDK/fetch-data.ts
@@ -0,0 +1,25 @@
+let BASE_URL = 'https://api.themoviedb.org/3'; // New API endpoint
+
+if (typeof process !== 'undefined' && process.env.BASE_URL) {
+  BASE_URL = process.env.BASE_URL;
+}
+
+async function fetchData(endpoint: string): Promise<any> {
+  try {
+    const response = await fetch(`${BASE_URL}/${endpoint}`, {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MmMxNzExN2M5MmE5ZmM5OGE3MDJkNzE2ZWY5YTlkMyIsInN1YiI6IjY2MjUwMzUwNjJmMzM1MDE0YmQ3YWI3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jsjvjdRN_ARHUQGYttVqgOdN7DTf206dtg1osQW2dAM'
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from endpoint: ${endpoint}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching data from endpoint ${endpoint}:`, error);
+    throw error;
+  }
+}
+
+export default fetchData;
--- packages/IMDbOT-SDK/index.ts
+++ packages/IMDbOT-SDK/index.ts
@@ -1,26 +1,16 @@
-const BASE_URL = process.env.BASE_URL || 'https://search.imdbot.workers.dev';
+import fetchData from './fetch-data';
 
-async function fetchData(endpoint: string): Promise<any> {
-  try {
-      const response = await fetch(`${BASE_URL}/${endpoint}`);
-      if (!response.ok) {
-          throw new Error(`Failed to fetch data from endpoint: ${endpoint}`);
-      }
-      return await response.json();
-  } catch (error) {
-      console.error(`Error fetching data from endpoint ${endpoint}:`, error);
-      throw error;
-  }
+// Search movies by query
+export async function searchMovie(query: string): Promise<any> {
+  return fetchData(`search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`);
 }
 
 // Search movies by query
-export async function getMovieData(query: string): Promise<any> {
-  return fetchData(`?q=${encodeURIComponent(query)}`);
+export async function getMovieData(): Promise<any> {
+  return fetchData(`discover/movie?include_adult=false&sort_by=popularity.desc&page=1&with_people`);
 }
 
-// Get more details by IMDb ID
-export async function getMovieDetails(imdbId: string): Promise<any> {
-  return fetchData(`?tt=${imdbId}`);
+// Get more details by ID
+export async function getMovieDetails(id: number): Promise<any> {
+  return fetchData(`movie/${id}`);
 }
-
-
