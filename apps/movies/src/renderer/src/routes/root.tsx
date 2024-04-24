1f9fd64a13338abfc6b1804c5965d41990faca2d
vvvrdo <vvvrdo@gmail.com>
2024-04-25 00:07:38 +0400

    UI/UX Improvements, New components, Improve component separation, Implement handling for empty movie lists, More loading indicators"

--- apps/movies/src/renderer/src/routes/root.tsx
+++ apps/movies/src/renderer/src/routes/root.tsx
@@ -1,21 +1,39 @@
-import { useEffect, useState } from 'react'
-import { useAppDispatch, useAppSelector } from '../hooks'
-import { fetchMovies } from '../features/movies/slice'
-import { Movie } from '../features/movies/types'
+import { useEffect } from 'react'
+import { useAppDispatch } from '../hooks'
+import {
+  clearMovieDetails,
+  fetchMovies,
+  selectLoading,
+  selectMovies,
+  selectSearch
+} from '../features/movies/slice'
 import Gallery from '@renderer/components/gallery'
+import ToastComponent from '@renderer/utils/error'
+import { useSelector } from 'react-redux'
+import NoResultsMessage from '@renderer/components/no-result'
+import { LoadingSpinner } from '@renderer/components/spinners'
 
 export default function Root(): JSX.Element {
-  const moviesData = useAppSelector((state) => state.movies.movies)
-  const [movies, setMovies] = useState<Movie[]>(moviesData)
+  const movies = useSelector(selectMovies)
+  const search = useSelector(selectSearch)
+  const loading = useSelector(selectLoading)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(fetchMovies())
-  }, [])
+    dispatch(clearMovieDetails())
+  }, [dispatch])
 
-  useEffect(() => {
-    setMovies(moviesData)
-  }, [moviesData])
-
-  return <Gallery movies={movies} />
+  return (
+    <>
+      <ToastComponent />
+      {movies.length > 0 ? (
+        <Gallery movies={movies} loading={loading} />
+      ) : search ? (
+        <NoResultsMessage message="No movies found" />
+      ) : (
+        <LoadingSpinner />
+      )}
+    </>
+  )
 }
